b19761ca261b36eec00c4089201f11d5ef586bcd
Julian <jb@futureplay.de>
2017-12-01 22:59:40 +0100

    greeting

--- ram.go
+++ ram.go
@@ -35,4 +35,15 @@ func main() {
 
 func ready(s *discordgo.Session, event *discordgo.Ready) {
 	s.UpdateStatus(0, "CANCER")
+	channels, err := s.GuildChannels("258726617361285131")
+	utils.CheckError(err, "Couldn't get the Server with the ID: ", "283066637928890379")
+
+	for _, channel := range channels {
+		go func(c *discordgo.Channel) {
+			fmt.Println(c.Name)
+			if c.Name == "dumb-bot-shit" {
+				_, _ = s.ChannelMessageSend(c.ID, "I HAVE RETURNED WITH MORE CANCER")
+			}
+		}(channel)
+	}
 }
