4dee4175fb38667ad5849b7a3b3440b76de4e2b9
Julian <jb@futureplay.de>
2017-12-02 00:01:06 +0100

    added a function to search for a user in a userarray

--- utils/util.go
+++ utils/util.go
@@ -1,10 +1,28 @@
 package utils
 
-import "log"
+import (
+	"log"
+
+	"github.com/bwmarrin/discordgo"
+)
 
 func CheckError(e error, msg ...interface{}) {
 	if e != nil {
 		log.Fatal(msg)
 		return
 	}
+}
+
+func MentionsContainsUser(arr []*discordgo.User, el *discordgo.User) bool {
+	if len(arr) == 0 {
+		return false
+	}
+
+	for _, element := range arr {
+		if element.ID == el.ID {
+			return true
+		}
+	}
+
+	return false
 }
