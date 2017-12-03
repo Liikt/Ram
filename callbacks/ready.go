95996548d52687cb485e58573de4c5d163b4bebb
Julian <jb@futureplay.de>
2017-12-03 14:36:07 +0100

    removed the message on startup for now for development reasons

--- callbacks/ready.go
+++ callbacks/ready.go
@@ -1,20 +1,17 @@
 package callbacks
 
-import (
-	utils "../utils"
-	"github.com/bwmarrin/discordgo"
-)
+import "github.com/bwmarrin/discordgo"
 
 func Ready(s *discordgo.Session, event *discordgo.Ready) {
 	s.UpdateStatus(0, "CANCER")
-	channels, err := s.GuildChannels("258726617361285131")
-	utils.CheckError(err, "Couldn't get the Server with the ID: ", "283066637928890379")
+	// channels, err := s.GuildChannels("258726617361285131")
+	// utils.CheckError(err, "Couldn't get the Server with the ID: ", "283066637928890379")
 
-	for _, channel := range channels {
-		go func(c *discordgo.Channel) {
-			if c.Name == "dumb-bot-shit" {
-				_, _ = s.ChannelMessageSend(c.ID, "I HAVE RETURNED WITH MORE CANCER")
-			}
-		}(channel)
-	}
+	// for _, channel := range channels {
+	// 	go func(c *discordgo.Channel) {
+	// 		if c.Name == "dumb-bot-shit" {
+	// 			_, _ = s.ChannelMessageSend(c.ID, "I HAVE RETURNED WITH MORE CANCER")
+	// 		}
+	// 	}(channel)
+	// }
 }
